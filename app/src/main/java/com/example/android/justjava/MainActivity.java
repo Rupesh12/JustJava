92ec9db5fcee0de9ff7021ff8179d955adeb605f
Rupesh12 <rupeshsoni001@gmail.com>
2016-08-07 16:19:40 -0400

    Add toast message functionality

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.justjava;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -13,6 +18,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
     int quantity = 0 ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,35 +29,74 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity*5 ;
-        String priceMessage = "Total: $ "+ price+"\nThank you !";
-        displayMessage(priceMessage);
+
+        CheckBox whipped_cream_topping = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        CheckBox choclate = (CheckBox) findViewById(R.id.choclate_checkbox);
+        EditText name_text = (EditText) findViewById(R.id.name) ;
+        String name = name_text.getText().toString() ;
+        boolean cream = whipped_cream_topping.isChecked();
+        boolean choc = choclate.isChecked();
+        //String priceMessage = "Total: $ "+ price+"\nThank you !";
+
+        int price = price(cream,choc) ;
+        Log.v("MainActivity.java","name "+name);
+        displayMessage(createOrderSummary(price,cream,choc,name));
+    }
+    /**
+     * Method to calculate the price
+     * **/
+    private int price(boolean cream , boolean choc){
+        int unit_price = 5 ;
+        if(cream){
+            unit_price++ ;
+        }
+        if(choc){
+            unit_price = unit_price + 2 ;
+        }
+        return quantity * unit_price ;
+    }
+    /**
+     * Method to create the order summary
+     * @param price of the coffe
+     * @return orderSummary
+     * **/
+    private String createOrderSummary(int price,boolean cream,boolean choclate, String name){
+        return ("Name : "+name+"\nAdd Whipped Cream : " + cream+"\nAdd Choclate : "+choclate+"\nQuantity : "+quantity+"\nTotal : $ "+price+"\nThank You");
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-
-    }
+    
 
     public void increment(View view) {
+        if(quantity<99)
+        displayQuantity(++quantity);
+        else{
+            Context context = getApplicationContext();
+            CharSequence text = "Sorry we can't let you die so soon.";
+            int duration = Toast.LENGTH_SHORT;
 
-        display(++quantity);
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
     public void decrement(View view) {
-        if(quantity>=1)
-         display(--quantity);
+        if(quantity>0)
+            displayQuantity(--quantity);
+        else{
+            Context context = getApplicationContext();
+            CharSequence text = "We don't make negative coffees";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
 
@@ -59,7 +104,12 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(message);
     }
+
+
+
+
+
 }
